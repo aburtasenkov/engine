07dd2d2e2f28715ad05be9b0f8daf648eb64590c
aburtasenkov <aburtasenkov@gmail.com>
2025-04-17 17:12:55 +0200

    Added source code files into compile statement

--- compile.bash
+++ compile.bash
@@ -1,2 +1,8 @@
 echo "Compiling source code..."
-g++ -o main main.cpp source/dependancy/glad.c -lGL -lglfw
+g++ -o main main.cpp \
+            source/dependancy/glad.c \
+            source/constants.cpp \
+            source/camera.cpp \
+            source/light.cpp \
+            source/shader.cpp \
+            -lGL -lglfw
