43379679e611320338d4f69385dbbeb182f33e6c
aburtasenkov <aburtasenkov@gmail.com>
2025-04-17 16:56:25 +0200

    Moved light's source code into cpp file

--- source/light.hpp
+++ source/light.hpp
@@ -16,23 +16,11 @@ class LightSource {
     float linear = 0.09f;
     float quadratic = 0.032f;
 
-    LightSource() {
-      kind = LightKind::Undefined;
-      vector = glm::vec3(1.0f);
-      color = glm::vec3(1.0f);
-    }
+    LightSource();
+    LightSource(LightKind k, glm::vec3 lightVector = ORIGIN, glm::vec3 lightColor = DEFAULT_COLOR);
 
-    LightSource(LightKind k, glm::vec3 lightVector = ORIGIN, glm::vec3 lightColor = DEFAULT_COLOR)
-      :kind(k), vector(lightVector), color(lightColor)
-    {    }
-
-    void updateVec(glm::vec3 newVector) {
-      vector = newVector;
-    }
-
-    void updateColor(glm::vec3 newColor) {
-      color = newColor;
-    }
+    void updateVec(glm::vec3 newVector);
+    void updateColor(glm::vec3 newColor);
 
   private:
     static constexpr glm::vec3 DEFAULT_COLOR = glm::vec3(1.0f, 1.0f, 1.0f);
